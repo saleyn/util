e07abc84429b289bd5f05aa9f056be445c2bd0b0
Serge Aleynikov <saleyn@gmail.com>
2021-08-17 15:54:19 -0400

    Documentation updated

--- build-aux/md-to-edoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ $# -ne 1 ] && echo "Usage: ${0##*/} Filename.md" && exit 1
-
-SCRIPT=$(readlink -f $0)
-AWK=${SCRIPT%.sh}.awk
-
-gawk -f ${AWK} vsn=$(git describe master --always --tags) "$1"
