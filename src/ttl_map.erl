7d0112a954fcc367f8094fb8a3ab982a93d99329
Serge Aleynikov <saleyn@gmail.com>
2023-04-16 23:15:59 -0400

    Add docs

--- src/ttl_map.erl
+++ src/ttl_map.erl
@@ -31,18 +31,30 @@
 %% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %%------------------------------------------------------------------------------
 -module(ttl_map).
--export([new/1, new/2, try_add/4, size/1, refresh/2, now/0]).
+-author('saleyn@gmail.com').
+
+-export([new/1, new/2, try_add/4, size/1, evict/2, evict/3, now/0]).
 
 -compile({no_auto_import,[now/0, size/1]}).
 
 -record(ttl_map, {ets, q, ttl}).
 
+-type ttl_map() :: #ttl_map{}.
+
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+%% @doc Create a new map with a given TTL time for inserted items
 new(TTL) ->
   new(TTL, #{}).
+
+%% @doc Create a new map with a given TTL time for inserted items.
+%% `Opts' is a list of options:
+%% <dl>
+%% <dt>name</dt><dd>The name of the ETS table (defaults to `undefined')</dd>
+%% <dt>access</dt><dd>The access level of the ETS table (defaults to `private')</dd>
+%% </dl>
 new(TTL, Opts) when is_integer(TTL), is_map(Opts) ->
   Name = maps:get(name,   Opts, undefined),
   ACL  = maps:get(access, Opts, private),
@@ -53,8 +65,13 @@ new(TTL, Opts) when is_integer(TTL), is_map(Opts) ->
     end,
   #ttl_map{ets = ets:new(Name, EtsOpts), q = queue:new(), ttl = TTL}.
 
+%% @doc Try to add a `Key/Value' pair to the map.
+%% If more than TTL time elapsed since the last insert of the `Key' or the
+%% `Key' is not found in the map, the value is inserted, otherwise no modifications
+%% are made.
+-spec try_add(ttl_map(), any(), any(), non_neg_integer()) -> {ttl_map(), Inserted::boolean()}.
 try_add(TTLMap = #ttl_map{ets = ETS, q = Q}, Key, Value, Now) when is_integer(Now) ->
-  TTLMap1 = refresh(TTLMap, Now),  %% Evict stale entries from the ETS
+  TTLMap1 = evict(TTLMap, Now),  %% Evict stale entries from the ETS
   case ets:lookup(ETS, Key) of
     [] ->
       ets:insert(ETS, {Key, {Value, Now}}),
@@ -63,18 +80,34 @@ try_add(TTLMap = #ttl_map{ets = ETS, q = Q}, Key, Value, Now) when is_integer(No
       {TTLMap1, false}
   end.
 
-refresh(TTLMap = #ttl_map{ets = ETS, q = Q, ttl = TTL}, Now) ->
+%% @doc Evict stale items from the map given the current timestamp `Now'.
+-spec evict(ttl_map(), non_neg_integer()) -> ttl_map().
+evict(TTLMap = #ttl_map{ets = ETS, q = Q, ttl = TTL}, Now) ->
   Threshold = Now - TTL,
-  case dropwhile(Q, queue:peek(Q), ETS, 0, Threshold) of
-    {_, 0} ->
-      TTLMap;
-    {V, _} ->
-      TTLMap#ttl_map{q = V}
+  Size      = size(TTLMap),
+  {R, Q1}   = peek(Q),
+  case dropwhile(Q1, R, ETS, 0, Threshold, Size) of
+    {_,  0} -> TTLMap#ttl_map{q = Q1};
+    {Q2, _} -> TTLMap#ttl_map{q = Q2}
   end.
 
+%% @doc Evict stale items (up to the `Limit') from the map given the current timestamp `Now'.
+-spec evict(ttl_map(), non_neg_integer(), non_neg_integer()) -> ttl_map().
+evict(TTLMap = #ttl_map{ets = ETS, q = Q, ttl = TTL}, Now, Limit) ->
+  Threshold = Now - TTL,
+  {R, Q1}   = peek(Q),
+  case dropwhile(Q1, R, ETS, 0, Threshold, Limit) of
+    {_,  0} -> TTLMap#ttl_map{q = Q1};
+    {Q2, _} -> TTLMap#ttl_map{q = Q2}
+  end.
+
+%% @doc Get the number of items in the map.
+-spec size(ttl_map()) -> non_neg_integer().
 size(#ttl_map{ets = ETS}) ->
   ets:info(ETS, size).
 
+%% @doc Get the current timestamp in microseconds since Unix epoch.
+-spec now() -> non_neg_integer().
 now() ->
   erlang:system_time(microsecond).
 
@@ -82,12 +115,24 @@ now() ->
 %% Internal functions
 %%------------------------------------------------------------------------------
 
-dropwhile(Q, {value, {Time, Key}}, ETS, N, Threshold) when Time =< Threshold ->
+dropwhile(Q, {value, {Time, Key}}, ETS, N, Threshold, I) when Time =< Threshold, I > 0 ->
   ets:delete(ETS, Key),
-  Q1 = queue:drop(Q),
-  dropwhile(Q1, queue:peek(Q1), ETS, N+1, Threshold);
-dropwhile(Q, _, _, N, _) ->
+  Q1        = queue:drop(Q),  %% Evict first element from the queue
+  {Res, Q2} = peek(Q1),       %% Pick the next element in the queue
+  dropwhile(Q2, Res, ETS, N+1, Threshold, I-1);
+dropwhile(Q, _, _, N, _, _) ->
   {Q, N}.
+
+%% We use this implementation of `peek' instead of the `queue:peek/1' because
+%% the later will occasionally call `lists:reverse/1' when the second list in
+%% the queue is empty, and wouldn't update the queue, whereas we want to memorize
+%% the result of reversal if one has been made.
+peek({_, [V|_]}=Q) -> {{value, V}, Q};
+peek({[], []}=Q)   -> {undefined,  Q};
+peek({[V],[]}=Q)   -> {{value, V}, Q};
+peek({[Y|In],[]}) ->
+    [V|_] = L = lists:reverse(In, []),
+    {{value,V}, {[Y], L}}.
 
 %%------------------------------------------------------------------------------
 %% Unit testing
@@ -108,18 +153,17 @@ add_test() ->
     {Map3, R3} = ttl_map:try_add(Map2, 1, 124, 5999),
     ?assertEqual(2, ttl_map:size(Map3)),
     ?assertNot(R3),
-    Map4 = ttl_map:refresh(Map3, 6000),
+    Map4 = ttl_map:evict(Map3, 6000),
     ?assertEqual(1, ttl_map:size(Map4)),
     {Map5, R5} = ttl_map:try_add(Map4, 1, 124, 6000),
     ?assertEqual(2, ttl_map:size(Map5)),
     ?assert(R5),
-    Map6 = ttl_map:refresh(Map5, 6500),
+    Map6 = ttl_map:evict(Map5, 6500),
     ?assertEqual(1, ttl_map:size(Map6)),
-    Map7 = ttl_map:refresh(Map6, 7001),
+    Map7 = ttl_map:evict(Map6, 7001),
     ?assertEqual(0, ttl_map:size(Map7)),
     ?assertEqual(queue:new(), Map7#ttl_map.q),
     ok.
   %end.
 
 -endif.
-
